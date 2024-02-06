eaf4f47db4897d5617e45425336866ff1b390526
David NAISSE <dnaisse@dalet.com>
2024-02-06 16:24:47 -0500

    initial commit
    - [added] .gitignore
    - [added] README.md

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from zenpy import Zenpy
 
+# load .env
 load_dotenv(find_dotenv())
 
 creds = {
@@ -12,12 +13,17 @@ creds = {
     'subdomain': os.environ.get('ZD_SUBDOMAIN')
 }
 
+# rate limiting of 200
 zenpy_client = Zenpy(proactive_ratelimit=200, **creds)
 
-tickets = zenpy_client.search(type='ticket', tags=['galaxy'])
+# this will retrieve **ALL** tickets
+tickets = zenpy_client.search(type='ticket')
+# galaxy_tickets = zenpy_client.search(type='ticket', tags=['galaxy'])
+# flex_tickets = zenpy_client.search(type='ticket', tags=['flex'])
 
-print(len(tickets))
+print(f"Tickets found: {len(tickets)}.")
 
+# iterate through tickets
 for ticket in tickets:
+    # do your stuff here
     print(json.dumps(ticket.to_dict(), indent=4))
-    break
